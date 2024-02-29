3110c34fcc42b298eeeba1f4a896e0c51b11dc46
kaiden-ong <kaiden.ong000@gmail.com>
2024-02-29 02:41:31 -0800

    Navbar complete

--- app/src/main/java/edu/uw/ischool/kong314/heartbeats/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/kong314/heartbeats/MainActivity.kt
@@ -1,19 +1,48 @@
 package edu.uw.ischool.kong314.heartbeats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.bson.Document
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
-import com.mongodb.ServerApi
-import com.mongodb.ServerApiVersion
-import com.mongodb.kotlin.client.coroutine.MongoClient
-import kotlinx.coroutines.runBlocking
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    val homeFragment = HomeFragment()
+    val mapFragment = MapFragment()
+    val postFragment = PostFragment()
+    val dailyFragment = DailyFragment()
+    val leaderboardFragment = LeaderboardFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navbar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        supportFragmentManager.beginTransaction().replace(R.id.container,homeFragment).commit()
+
+        navbar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container,homeFragment).commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.map -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container,mapFragment).commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.post -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container,postFragment).commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.daily -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container,dailyFragment).commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.leaderboard -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container,leaderboardFragment).commit()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
---
+++ app/src/main/java/edu/uw/ischool/kong314/heartbeats/PostFragment.kt
@@ -0,0 +1,13 @@
+package edu.uw.ischool.kong314.heartbeats
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PostFragment() : Fragment(R.layout.fragment_post)  {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
