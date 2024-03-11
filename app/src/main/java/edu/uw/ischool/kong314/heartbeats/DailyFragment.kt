58f11818da231cc25e468ba6f77be932e10d062d
ryanngu023 <ryannguyen10023@gmail.com>
2024-03-10 18:35:37 -0700

    daily prompt changes

--- app/src/main/java/edu/uw/ischool/kong314/heartbeats/DailyFragment.kt
+++ app/src/main/java/edu/uw/ischool/kong314/heartbeats/DailyFragment.kt
@@ -1,22 +1,37 @@
 package edu.uw.ischool.kong314.heartbeats
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import kotlinx.coroutines.runBlocking
-import android.util.Log
-
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Locale
 
 
 class DailyFragment() : Fragment(R.layout.fragment_daily)  {
     private lateinit var heartbeatsApp: HeartbeatsApp
     private lateinit var databaseRepo: DatabaseRepository
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var challengesList: List<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        sharedPreferences = requireContext().getSharedPreferences("DailyFragmentPrefs", Context.MODE_PRIVATE)
+
+        val currentTime = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault())
+        val fDate = dateFormat.format(currentTime)
+
+        val dateText = view.findViewById<TextView>(R.id.dailyDate)
+        dateText.text = fDate
+
+        val promptText = view.findViewById<TextView>(R.id.textView4)
 
         heartbeatsApp = requireActivity().application as HeartbeatsApp
         databaseRepo = heartbeatsApp.databaseRepository
@@ -26,9 +41,12 @@ class DailyFragment() : Fragment(R.layout.fragment_daily)  {
             } else {
                 if (challenges != null) {
                     println("Retrieved challenges: $challenges")
+                    challengesList = challenges
                 } else {
                     println("No challenges found.")
                 }
+                val challengeIdx = (LocalDate.now().toEpochDay() % challengesList.size).toInt()
+                promptText.text = challengesList.get(challengeIdx)
             }
         }
 
